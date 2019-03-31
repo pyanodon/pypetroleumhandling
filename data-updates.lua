3bdc258c2d636512ea64c2f5eb1394dee3b31fbd
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-03-30 22:26:43 -0400

    mergeing

---
+++ data-updates.lua
@@ -0,0 +1,28 @@
+require("__stdlib__/stdlib/data/data").Util.create_data_globals()
+
+if mods["pyfusionenergy"] then
+    require("prototypes/updates/pyfusionenergy-updates")
+end
+
+if mods["pyhightech"] then
+    require("prototypes/updates/pyhightech-updates")
+end
+
+if mods["pycoalprocessing"] then
+    require("prototypes/updates/pycoalprocessing-updates")
+end
+
+if mods["pyrawores"] then
+    require("prototypes/updates/pyrawores-updates")
+end
+
+if mods["pyindustry"] then
+    require("prototypes/updates/pyindustry-updates")
+end
+
+--ADAPTATIONS
+
+
+--RECIPES UPDATES
+
+RECIPE("electric-engine-unit"):add_ingredient({type = "item", name = "small-parts-02", amount = 10})
