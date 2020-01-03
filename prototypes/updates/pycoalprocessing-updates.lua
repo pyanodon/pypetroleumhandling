587a5f4f126e803a330f812414f70d95939da485
pyanodon <smylodon@gmail.com>
2020-01-03 11:20:58 -0300

    small thing

--- prototypes/updates/pycoalprocessing-updates.lua
+++ prototypes/updates/pycoalprocessing-updates.lua
@@ -183,7 +183,7 @@ RECIPE {
         {type = "fluid", name = "water", amount = 500}
     },
     results = {
-        {type = "fluid", name = "combustion-mixture1", amount = 100, temperature = 600},
+        {type = "fluid", name = "combustion-mixture1", amount = 100, temperature = 650},
         {type = "fluid", name = "steam", amount = 500, temperature = 60}
     },
     icon = "__pypetroleumhandling__/graphics/icons/combustion-naphtha.png",
