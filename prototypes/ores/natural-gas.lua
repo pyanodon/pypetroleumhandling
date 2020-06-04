572cdeddad180ab8335760238ba34989f4f87ee5
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-06-04 04:59:00 -0400

    finish up changes to the oil-gas swap scripts
    Former-commit-id: 0f026d2c8083bf4291c34d47b9f507e4ea6e3880
    Former-commit-id: 0491e4b2ea0b98917ab8752a6e1edac6d84944e9

--- prototypes/ores/natural-gas.lua
+++ prototypes/ores/natural-gas.lua
@@ -5,17 +5,24 @@ local nat_gas3 = table.deepcopy(data.raw.resource['oil-mk03'])
 local nat_gas4 = table.deepcopy(data.raw.resource['oil-mk04'])
 
 nat_gas1.name = 'natural-gas-1'
-nat_gas1.minable.results.name = 'natural-gas'
+nat_gas1.minable.results[1].name = 'natural-gas'
 nat_gas1.autoplace = {}
 
 nat_gas2.name = 'natural-gas-2'
-nat_gas2.minable.results.name = 'natural-gas'
+nat_gas2.minable.results[1].name = 'natural-gas'
 nat_gas2.autoplace = {}
 
 nat_gas3.name = 'natural-gas-3'
-nat_gas3.minable.results.name = 'natural-gas'
+nat_gas3.minable.results[1].name = 'natural-gas'
 nat_gas3.autoplace = {}
 
 nat_gas4.name = 'natural-gas-4'
-nat_gas4.minable.results.name = 'natural-gas'
+nat_gas4.minable.results[1].name = 'natural-gas'
 nat_gas4.autoplace = {}
+
+log(serpent.block(data.raw.resource['natural-gas-1']))
+data.raw.resource['natural-gas-1'] = nat_gas1
+data.raw.resource['natural-gas-2'] = nat_gas2
+data.raw.resource['natural-gas-3'] = nat_gas3
+data.raw.resource['natural-gas-4'] = nat_gas4
+log(serpent.block(data.raw.resource['natural-gas-1']))
