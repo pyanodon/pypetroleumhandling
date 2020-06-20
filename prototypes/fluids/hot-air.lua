eb8b0fc5bdf2ebca6cc8bab75d960f53657177a4
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-06-20 06:07:46 -0400

    tweaks

--- prototypes/fluids/hot-air.lua
+++ prototypes/fluids/hot-air.lua
@@ -1,24 +1,3 @@
-RECIPE {
-    type = "recipe",
-    name = "hot-air",
-    category = "rhe",
-    --hidden = true,
-    enabled = false,
-    energy_required = 2,
-    ingredients = {
-        {type = "item", name = "hot-stone-brick", amount = 10},
-        {type = "fluid", name = "pressured-air", amount = 150},
-    },
-    results = {
-        {type = "fluid", name = "hot-air", amount = 400},
-        {type = "item", name = "stone-brick", amount = 10},
-    },
-    main_product = "hot-air",
-    icon = "__pypetroleumhandlinggraphics__/graphics/icons/hot-air.png",
-    icon_size = 32,
-    subgroup = "py-petroleum-handling-recipes",
-    order = "a2"
-}:add_unlock("coal-processing-1")
 
 RECIPE {
     type = "recipe",
--- prototypes/recipes/hot-stone-brick-recipes.lua
+++ prototypes/recipes/hot-stone-brick-recipes.lua
@@ -7,18 +7,17 @@ RECIPE {
     energy_required = 3,
     ingredients = {
         {type = "item", name = "stone-brick", amount = 10},
-        {type = "fluid", name = "coke-oven-gas", amount = 100, temperature = 500},
+        {type = "fluid", name = "outlet-gas-04", amount = 100, minimum_temperature = 500},
     },
     results = {
         {type = "item", name = "warm-stone-brick", amount = 10},
-        {type = "fluid", name = "coke-oven-gas", amount = 100, temperature = 250},
-
+        {type = "fluid", name = "outlet-gas-04", amount = 100, temperature = 250},
     },
     subgroup = "py-petroleum-handling-recipes",
     order = "a2",
     icon = "__pypetroleumhandlinggraphics__/graphics/icons/hot-stone-brick-recipe.png",
     icon_size = 32,
-}:add_unlock("coal-processing-1")
+}:add_unlock("coke-mk03")
 
 RECIPE {
     type = "recipe",
@@ -28,17 +27,17 @@ RECIPE {
     energy_required = 3,
     ingredients = {
         {type = "item", name = "warm-stone-brick", amount = 10},
-        {type = "fluid", name = "coke-oven-gas", amount = 100, temperature = 750},
+        {type = "fluid", name = "outlet-gas-04", amount = 100, minimum_temperature = 750},
     },
     results = {
         {type = "item", name = "warmer-stone-brick", amount = 10},
-        {type = "fluid", name = "coke-oven-gas", amount = 100, temperature = 500},
+        {type = "fluid", name = "outlet-gas-04", amount = 100, temperature = 500},
     },
     subgroup = "py-petroleum-handling-recipes",
     order = "a2",
     icon = "__pypetroleumhandlinggraphics__/graphics/icons/hot-stone-brick-recipe.png",
     icon_size = 32,
-}:add_unlock("coal-processing-1")
+}:add_unlock("coke-mk03")
 
 RECIPE {
     type = "recipe",
@@ -48,15 +47,80 @@ RECIPE {
     energy_required = 3,
     ingredients = {
         {type = "item", name = "warmer-stone-brick", amount = 10},
-        {type = "fluid", name = "coke-oven-gas", amount = 100, temperature = 1000},
+        {type = "fluid", name = "outlet-gas-04", amount = 100, minimum_temperature = 1000},
     },
     results = {
         {type = "item", name = "hot-stone-brick", amount = 10},
-        {type = "fluid", name = "coke-oven-gas", amount = 100, temperature = 750},
+        {type = "fluid", name = "outlet-gas-04", amount = 100, temperature = 750},
     },
     subgroup = "py-petroleum-handling-recipes",
     order = "a2",
     icon = "__pypetroleumhandlinggraphics__/graphics/icons/hot-stone-brick-recipe.png",
     icon_size = 32,
-}:add_unlock("coal-processing-1")
+}:add_unlock("coke-mk03")
 
+RECIPE {
+    type = "recipe",
+    name = "hot-air-1",
+    category = "rhe",
+    --hidden = true,
+    enabled = false,
+    energy_required = 2,
+    ingredients = {
+        {type = "item", name = "warm-stone-brick", amount = 10},
+        {type = "fluid", name = "pressured-air", amount = 150},
+    },
+    results = {
+        {type = "fluid", name = "hot-air", amount = 200},
+        {type = "item", name = "stone-brick", amount = 10},
+    },
+    main_product = "hot-air",
+    icon = "__pypetroleumhandlinggraphics__/graphics/icons/hot-air.png",
+    icon_size = 32,
+    subgroup = "py-petroleum-handling-recipes",
+    order = "a2"
+}:add_unlock("coke-mk03")
+
+RECIPE {
+    type = "recipe",
+    name = "hot-air-2",
+    category = "rhe",
+    --hidden = true,
+    enabled = false,
+    energy_required = 2,
+    ingredients = {
+        {type = "item", name = "warmer-stone-brick", amount = 10},
+        {type = "fluid", name = "pressured-air", amount = 150},
+    },
+    results = {
+        {type = "fluid", name = "hot-air", amount = 600},
+        {type = "item", name = "stone-brick", amount = 10},
+    },
+    main_product = "hot-air",
+    icon = "__pypetroleumhandlinggraphics__/graphics/icons/hot-air.png",
+    icon_size = 32,
+    subgroup = "py-petroleum-handling-recipes",
+    order = "a2"
+}:add_unlock("coke-mk03")
+
+RECIPE {
+    type = "recipe",
+    name = "hot-air-3",
+    category = "rhe",
+    --hidden = true,
+    enabled = false,
+    energy_required = 2,
+    ingredients = {
+        {type = "item", name = "hot-stone-brick", amount = 10},
+        {type = "fluid", name = "pressured-air", amount = 150},
+    },
+    results = {
+        {type = "fluid", name = "hot-air", amount = 1000},
+        {type = "item", name = "stone-brick", amount = 10},
+    },
+    main_product = "hot-air",
+    icon = "__pypetroleumhandlinggraphics__/graphics/icons/hot-air.png",
+    icon_size = 32,
+    subgroup = "py-petroleum-handling-recipes",
+    order = "a2"
+}:add_unlock("coke-mk03")
