370e3922fe0b0f6c5420791596a5577f136b1c4a
pyanodon <smylodon@gmail.com>
2020-10-24 22:17:24 -0300

    updates

--- prototypes/updates/pyhightech-updates.lua
+++ prototypes/updates/pyhightech-updates.lua
@@ -51,6 +51,7 @@ RECIPE("pure-natural-gas"):replace_ingredient("active-carbon", "colloidal-silica
 RECIPE("drilling-fluid-3"):add_ingredient({type = "item", name = "collagen", amount = 2})
 RECIPE("small-parts-02"):add_ingredient({type = "item", name = "nylon", amount = 1})
 RECIPE("ht-pipes"):add_ingredient({type = "item", name = "rubber", amount = 2})
+RECIPE("ht-locomotive"):add_ingredient({type = "item", name = "small-parts-02", amount = 50})
 
 
 ----EXCLUSIVE RECIPES----
