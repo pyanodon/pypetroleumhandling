d3371d5261bdf63699ba4b81d192e5e06a909abb
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-11-07 19:27:27 -0500

    fix ethylene glycol

--- prototypes/fluids/ethylene-glycol.lua
+++ prototypes/fluids/ethylene-glycol.lua
@@ -1,7 +1,7 @@
 RECIPE {
     type = "recipe",
     name = "ethylene-glycol",
-    category = "pan", --pyro scrubber
+    category = "mixer", --pyro scrubber
     enabled = false,
     energy_required = 4,
     ingredients = {
