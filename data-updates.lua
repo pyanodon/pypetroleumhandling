f5dadc452eed1e350975c3078efcbc82dd0bb071
kingarthur91 <arthurkoy_durbin@yahoo.com>
2021-07-17 06:04:19 -0400

    Update data-updates.lua

--- data-updates.lua
+++ data-updates.lua
@@ -25,8 +25,8 @@ RECIPE("lab-instrument"):add_ingredient({type = "item", name = "small-parts-01",
 RECIPE("chemical-science-pack"):add_ingredient({type = "item", name = "rubber", amount = 10})
 RECIPE("electric-engine-unit"):add_ingredient({type = "item", name = "small-parts-02", amount = 10})
 RECIPE("engine-unit"):remove_ingredient("small-parts-01"):add_ingredient({type = "item", name = "small-parts-01", amount = 4}):set_fields {energy_required = 15}
-RECIPE("assembling-machine-2"):add_ingredient({type = "item", name = "small-parts-02", amount = 10})
-RECIPE("assembling-machine-3"):add_ingredient({type = "item", name = "small-parts-03", amount = 15})
+RECIPE("assembling-machine-2"):add_ingredient({type = "item", name = "small-parts-01", amount = 10})
+RECIPE("assembling-machine-3"):add_ingredient({type = "item", name = "small-parts-02", amount = 15})
 RECIPE("fast-transport-belt"):remove_ingredient("small-parts-01"):add_ingredient({type = "item", name = "small-parts-02", amount = 2})
 RECIPE("fast-underground-belt"):remove_ingredient("small-parts-01"):add_ingredient({type = "item", name = "small-parts-02", amount = 6})
 RECIPE("fast-splitter"):remove_ingredient("small-parts-01"):add_ingredient({type = "item", name = "small-parts-02", amount = 6})
