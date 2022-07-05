f808ac03b025c8741e333cdd1d41528d2b73ac9b
ShadowGlass0 <gkecskem@gmail.com>
2022-07-05 22:56:54 +1000

    Soot separation (#48)

--- prototypes/updates/pyindustry-updates.lua
+++ prototypes/updates/pyindustry-updates.lua
@@ -7,6 +7,8 @@ RECIPE("mk02-fluid-wagon"):add_ingredient({type = "item", name = "rubber", amoun
 RECIPE("py-roboport-mk02"):add_ingredient({type = "item", name = "small-parts-02", amount = 20})
 RECIPE("py-roboport-mk03"):add_ingredient({type = "item", name = "small-parts-03", amount = 50})
 
+table.insert(RECIPE("ash-separation").results, {type = "item", name = "soot", amount = 1, probability = 0.2})
+
 RECIPE {
     type = "recipe",
     name = "bitumen-to-asphalt",
@@ -27,3 +29,26 @@ RECIPE {
     order = "a-asphalt-b"
 }:add_unlock("oil-sands")
 
+RECIPE {
+    type = "recipe",
+    name = "soot-separation",
+    category = "solid-separator",
+    enabled = false,
+    energy_required = 1,
+    ingredients = {
+        {type = "item", name = "soot", amount = 1}
+    },
+    results = {
+        {type = "item", name = "iron-ore", amount = 1, probability = 0.2},
+        {type = "item", name = "copper-ore", amount = 1, probability = 0.2},
+        {type = "item", name = "ore-aluminium", amount = 1, probability = 0.1},
+        {type = "item", name = "ore-zinc", amount = 1, probability = 0.1},
+        {type = "item", name = "ore-lead", amount = 1, probability = 0.1},
+        {type = "item", name = "ash", amount = 1, probability = 0.3}
+    },
+    unlock_results = false,
+    icon = "__pycoalprocessinggraphics__/graphics/missing_icon_64.png",
+    icon_size = 64,
+    subgroup = "py-items-class",
+    order = "b"
+}:add_unlock("separation")
