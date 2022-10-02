edfb82d1114a68a4a41312a8af6131619ed2a160
kingarthur91 <arthurkoy_durbin@yahoo.com>
2022-10-01 21:38:53 -0400

    reduce duplicated code

--- prototypes/ores/natural-gas.lua
+++ prototypes/ores/natural-gas.lua
@@ -4,8 +4,7 @@ local nat_gas2 = table.deepcopy(data.raw.resource['crude-oil'])
 local nat_gas3 = table.deepcopy(data.raw.resource['crude-oil'])
 local nat_gas4 = table.deepcopy(data.raw.resource['crude-oil'])
 
-nat_gas1.name = 'natural-gas-mk01'
-nat_gas1.minable =
+local minable =
 {
   mining_time = 1,
   results =
@@ -17,60 +16,30 @@ nat_gas1.minable =
     }
   }
 }
+
+nat_gas1.name = 'natural-gas-mk01'
+nat_gas1.minable = minable
 nat_gas1.infinite = false
 nat_gas1.autoplace = nil
 nat_gas1.map_color = {r=0.2, g=0.1, b=0.2}
 nat_gas1.category = 'natural-gas'
 
 nat_gas2.name = 'natural-gas-mk02'
-nat_gas2.minable =
-{
-  mining_time = 1,
-  results =
-  {
-    {
-      type = "fluid",
-      name = "raw-gas",
-      amount = 100,
-    }
-  }
-}
+nat_gas2.minable = minable
 nat_gas2.infinite = false
 nat_gas2.autoplace = nil
 nat_gas1.map_color = {r=0.2, g=0.1, b=0.2}
 nat_gas2.category = 'natural-gas'
 
 nat_gas3.name = 'natural-gas-mk03'
-nat_gas3.minable =
-{
-  mining_time = 1,
-  results =
-  {
-    {
-      type = "fluid",
-      name = "raw-gas",
-      amount = 100,
-    }
-  }
-}
+nat_gas3.minable = minable
 nat_gas3.infinite = false
 nat_gas3.autoplace = nil
 nat_gas1.map_color = {r=0.2, g=0.1, b=0.2}
 nat_gas3.category = 'natural-gas'
 
 nat_gas4.name = 'natural-gas-mk04'
-nat_gas4.minable =
-{
-  mining_time = 1,
-  results =
-  {
-    {
-      type = "fluid",
-      name = "raw-gas",
-      amount = 100,
-    }
-  }
-}
+nat_gas4.minable = minable
 nat_gas4.infinite = false
 nat_gas4.autoplace = nil
 nat_gas1.map_color = {r=0.2, g=0.1, b=0.2}
