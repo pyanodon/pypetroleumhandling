dacad315ae2f03befd8bf95fb8974a3c5077dc3b
kingarthur91 <arthurkoy_durbin@yahoo.com>
2022-10-12 03:15:16 -0400

    Update small-parts.lua

--- prototypes/technologies/small-parts.lua
+++ prototypes/technologies/small-parts.lua
@@ -30,8 +30,8 @@ TECHNOLOGY {
         count = 200,
         ingredients = {
             {"automation-science-pack", 1},
-            --{"logistic-science-pack", 1},
-            --{"chemical-science-pack", 1},
+            {"logistic-science-pack", 1},
+            {"chemical-science-pack", 1},
         },
         time = 50
     }
