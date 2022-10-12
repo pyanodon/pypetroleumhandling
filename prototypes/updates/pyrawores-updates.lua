0d79938149225cd2823a0359c449150749f38619
kingarthur91 <arthurkoy_durbin@yahoo.com>
2022-10-12 00:52:00 -0400

    finally got pyph and pyro to play nice

--- prototypes/updates/pyrawores-updates.lua
+++ prototypes/updates/pyrawores-updates.lua
@@ -6,6 +6,8 @@ TECHNOLOGY("vanadium-processing"):remove_pack("chemical-science-pack")
 
 --BUILDINDS--
 
+RECIPE("xyhiphoe-pool-mk01"):replace_ingredient("advanced-circuit", "electronic-circuit")
+
 RECIPE("oil-sand-extractor-mk01"):add_ingredient({type = "item", name = "lead-plate", amount = 15}):add_ingredient({type = "item", name = "nexelit-plate", amount = 5})
 RECIPE("oil-sand-extractor-mk02"):add_ingredient({type = "item", name = "duralumin", amount = 40}):add_ingredient({type = "item", name = "titanium-plate", amount = 50})
 RECIPE("oil-sand-extractor-mk03"):add_ingredient({type = "item", name = "stainless-steel", amount = 20}):add_ingredient({type = "item", name = "aluminium-plate", amount = 40})
