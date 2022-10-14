52305b425a462523c0d1ac5296b7ce70b9e59f27
ShadowGlass0 <gkecskem@gmail.com>
2022-10-15 05:51:49 +1100

    Fix fracking tech units (#68)

--- prototypes/technologies/fracking.lua
+++ prototypes/technologies/fracking.lua
@@ -12,7 +12,7 @@ TECHNOLOGY {
             {'automation-science-pack', 1},
             {'logistic-science-pack', 1},
             {'chemical-science-pack', 1},
-            {"military-science-pack"},
+            {"military-science-pack", 1},
             {"production-science-pack", 1},
             {"utility-science-pack", 1}
         },
