9bdd5e2d55ca40b34f87cdc9b9c7d4784c0396b5
kingarthur91 <arthurkoy_durbin@yahoo.com>
2022-10-20 02:54:41 -0400

    fix tech tree issue

--- prototypes/updates/pycoalprocessing-updates.lua
+++ prototypes/updates/pycoalprocessing-updates.lua
@@ -50,7 +50,7 @@ if not (mods["pyrawores"] or mods["pyhightech"]) then
 end
 --BUILDINDS--
 
-RECIPE("olefin-plant"):remove_unlock("lubricant"):add_unlock("scrude"):replace_ingredient("advanced-circuit", "electronic-circuit")
+RECIPE("olefin-plant"):remove_unlock("lubricant"):add_unlock("scrude"):replace_ingredient("advanced-circuit", "electronic-circuit"):replace_ingredient("chemical-plant-mk01", "reformer-mk01")
 -- RECIPE("chemical-plant-mk01"):remove_unlock('filtration'):add_unlock('fluid-processing-machines-1')
 RECIPE("evaporator"):remove_unlock('fluid-processing-machines-1'):add_unlock('fluid-processing-machines-2')
 RECIPE("agitator-mk01"):remove_unlock('fluid-processing-machines-1'):add_unlock('fluid-processing-machines-2')
