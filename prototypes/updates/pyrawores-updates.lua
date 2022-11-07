f3a9d971254c1dbe20c64d15b18888eee4c081ce
kingarthur91 <arthurkoy_durbin@yahoo.com>
2022-11-06 21:27:24 -0500

    Revert "Revert "unbreak pyph+pyro""
    This reverts commit 5fbe2e6ac6df7f5e407108f9ff8e230d84bcdb87.

--- prototypes/updates/pyrawores-updates.lua
+++ prototypes/updates/pyrawores-updates.lua
@@ -110,7 +110,7 @@ RECIPE("natural-gas-seep"):replace_ingredient('distilator', 'distilator-mk02')
 RECIPE("natural-gas-seep-mk04"):replace_ingredient('distilator', 'distilator-mk04')
 RECIPE("bitumen-seep-mk04"):replace_ingredient('distilator', 'distilator-mk04')
 
-RECIPE("quenching-tower"):remove_unlock("machines-mk01"):add_unlock("nexelit-mk01")
+--RECIPE("quenching-tower"):remove_unlock("machines-mk01"):add_unlock("nexelit-mk01")
 
 --RECIPES--
 
