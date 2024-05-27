7ee045e03e04a54b86534ac3d7bf27f785476a6d
notnotmelon <zacharypicco@gmail.com>
2024-05-26 19:49:50 -0500

    Remove duplicate code. This already exists in pyro

--- prototypes/updates/pyrawores-updates.lua
+++ prototypes/updates/pyrawores-updates.lua
@@ -951,6 +951,4 @@ RECIPE('casting-niobium-pipe'):add_ingredient({type = "item", name = "rubber", a
 
 RECIPE('casting-niobium-pipe-underground'):add_ingredient({type = "item", name = "rubber", amount = 2})
 
-if mods.pystellarexpedition then RECIPE('casting-engine-unit'):add_ingredient({type = "item", name = "belt", amount = 5}) end
-
 RECIPE('desulfurizator-unit-mk04'):add_unlock('oil-machines-mk04')
