601906f4d1aea08d9abc953a884d7b9a9fb60cc7
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-09-15 12:56:51 +0200

    add base_flow_rate to fluids

--- prototypes/fluids/fuel-oil.lua
+++ prototypes/fluids/fuel-oil.lua
@@ -43,6 +43,7 @@ FLUID {
     icon = "__pypetroleumhandlinggraphics__/graphics/icons/fuel-oil.png",
     icon_size = 32,
     default_temperature = 10, -- less than 15 = liquid / equal a 15 = gas
+    base_flow_rate = 100,
     base_color = {r = 0.937, g = 0.611, b = 0.00},
     flow_color = {r = 1, g = 1, b = 1},
     max_temperature = 100,
--- prototypes/fluids/hot-syngas.lua
+++ prototypes/fluids/hot-syngas.lua
@@ -4,6 +4,7 @@ FLUID {
     icon = "__pypetroleumhandlinggraphics__/graphics/icons/hot-syngas.png",
     icon_size = 32,
     default_temperature = 15, -- less than 15 = liquid / equal a 15 = gas
+    base_flow_rate = 100,
     base_color = {r = 0.7, g = 0.464, b = 0.129},
     flow_color = {r = 1, g = 1, b = 1},
     max_temperature = 100,
