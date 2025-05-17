5157e2fa7175da75ac7382a9292632363587db62
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2025-05-17 23:41:41 +0200

    unhide functions from the profiler

--- control.lua
+++ control.lua
@@ -3,11 +3,11 @@ require "__pypostprocessing__.lib"
 require "scripts/wiki/text-pages"
 require "scripts/bitumen"
 
-remote.add_interface("pyph", {
-    ---@param func string
-    execute_on_nth_tick = function(func)
+py.on_event(defines.events.on_tick, function(event)
+    local func_list = remote.call("on_nth_tick", "query", "pyph", event.tick)
+    for _, func in pairs(func_list) do
         py.mod_nth_tick_funcs[func]()
     end
-})
+end)
 
 py.finalize_events()
