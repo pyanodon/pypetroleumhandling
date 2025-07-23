077d2d083119c2fbf247bee5c86efc5623127f8a
Alyssa Doubrovsky <60121943+adoubrovsky@users.noreply.github.com>
2025-07-23 08:33:00 -0700

    [HM] Hydrogen void unlocked in Automation science needs py1 #1066 (#110)
    * [HM] Hydrogen void unlocked in Automation science needs py1 tech to use #1066
    
    Ionic Upgrader unlocked at oil machines 01. Gasoline recipe hidden. Identical gasoline recipe crafted in the gasifier added. Added depreciation message for original recipe. Updated changelog.txt
    
    * Moved ionic upgrader back to being unlocked at syngas
    
    This is to ensure that there will be no breaking changes to any base.

--- prototypes/recipes/gasoline-recipes.lua
+++ prototypes/recipes/gasoline-recipes.lua
@@ -3,6 +3,25 @@ RECIPE {
     name = "anthracene-gasoline-hydrogenation",
     category = "upgrader",
     enabled = false,
+	hidden = true,
+    energy_required = 4,
+    ingredients = {
+        {type = "fluid", name = "anthracene-oil", amount = 50},
+        {type = "fluid", name = "hydrogen",       amount = 50}
+    },
+    results = {
+        {type = "fluid", name = "gasoline", amount = 25},
+    },
+    main_product = "gasoline",
+    subgroup = "py-petroleum-handling-recipes",
+    order = "a"
+}:add_unlock("syngas")
+
+RECIPE {
+    type = "recipe",
+    name = "anthracene-gasoline-hydrogenation-new",
+    category = "gasifier",
+    enabled = false,
     energy_required = 4,
     ingredients = {
         {type = "fluid", name = "anthracene-oil", amount = 50},
